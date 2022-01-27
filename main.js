3f328d2e02492b8f89dff1c26883e429533633fe
Saksham11october <75781755+Saksham11october@users.noreply.github.com>
2022-01-27 21:30:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+kisi_ki_muskurahoton_pe_ho_nisar = "";
+Tujshe_Naraaz_Nahin_Zindagi = "";
+
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.position(500, 200);
+
+    video = createCapture(VIDEO);
+    video.hide();
 }
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
